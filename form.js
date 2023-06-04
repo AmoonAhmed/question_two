6aef9123072cdcd407310c368929fbd8f1847a4e
AmoonAhmed <emant2010@hotmail.com>
2023-06-04 18:26:50 +0300

    added styles and script

--- form.js
+++ form.js
@@ -1,6 +1,8 @@
+
 function displayMessage(){
-    var FN = document.getElementById("FN");
-    var LN = document.getElementById("LN");
-    alert("Congratultions " + FN + LN + " you are now signed up!");
+    var FN = document.getElementById("FN").value;
+    var LN = document.getElementById("LN").value;
+    // console.log(FN+" "+LN);
+    alert("Congratultions " + FN +" "+ LN + " you are now signed up!");
 }
-document.getElementById("Button").addEventListener("click",displayMessage);
+
